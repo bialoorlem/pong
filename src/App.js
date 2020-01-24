080ff140bb9211bc0d3ec4a8005c2d442b671446
Elliot Trejo <elliot.trejo.tcu@gmail.com>
2020-01-24 13:57:12 -0600

    Updated the color scheme of the game.

--- src/App.js
+++ src/App.js
@@ -5,105 +5,102 @@ import Container from "./components/Container/Container";
 import Ball from "./components/Ball/Ball";
 
 const initialState = {
-  p1Paddle: {
-    y: 0
-  },
-  p2Paddle: {
-    y: 0
+  paddle: {
+    x: 0
   },
   ball: {
     x: 0,
     y: 0,
-    dx: 10,
+    dx: 5,
     dy: 5
   }
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "MOVE_PADDLE_P1":
-      return { ...state, p1Paddle: action.payload };
-    case "MOVE_PADDLE_P2":
-      return { ...state, p2Paddle: action.payload };
+    case "MOVE_PADDLE":
+      return { ...state, paddle: action.payload };
     case "MOVE_BALL":
       return { ...state, ball: action.payload };
     default:
-      throw new Error("mismatched event: " + action.type);
+      throw new Error();
   }
 }
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  // const [p1PaddleY, setP1PaddleY] = useState(0);
-  // const [p2PaddleY, setP2PaddleY] = useState(0);
-
-  function handleKey(e) {
-    // let boundedY;
-    // const offset = (window.innerHeight - 600) / 2;
-
-    const char = e.key.toLowerCase();
-    if (char === "a" || char === "z") {
-      dispatch({
-        type: "MOVE_PADDLE_P1",
-        payload: {
-          y: state.p1Paddle.y + (char === "a" ? -10 : 10)
-        }
-      });
+  function handleMouse(e) {
+    let boundedX;
+    const offset = (window.innerWidth - 300) / 2;
+    if (e.x - offset < 0) {
+      boundedX = 0;
+    } else if (e.x - offset > 200) {
+      boundedX = 200;
+    } else {
+      boundedX = e.x - offset;
     }
-    if (char === "'" || char === "/") {
-      dispatch({
-        type: "MOVE_PADDLE_P2",
-        payload: {
-          y: state.p2Paddle.y + (char === "'" ? -10 : 10)
-        }
-      });
-    }
+    dispatch({
+      type: "MOVE_PADDLE",
+      payload: {
+        x: boundedX
+      }
+    });
   }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
 
   useEffect(() => {
-    window.addEventListener("keydown", handleKey);
-    return () => window.removeEventListener("keydown", handleKey);
-  }, [state.p2Paddle, state.p1Paddle]);
-
-  useEffect(() => {
-    const handleBall = setTimeout(() => {
+    const handle = setTimeout(() => {
+      let x = state.ball.x;
+      let y = state.ball.y;
       let dx = state.ball.dx;
       let dy = state.ball.dy;
-      if (
-        state.ball.x + state.ball.dx > 900 - 20 ||
-        state.ball.x + state.ball.dx < 1
-      ) {
+
+      let paddleX = state.paddle.x;
+
+      if (y > 370) {
+        return dispatch({
+          type: "MOVE_BALL",
+          payload: {
+            dx: 5,
+            dy: 5,
+            x: 0,
+            y: 0
+          }
+        });
+      }
+
+      if (x + dx > 300 - 20 || x + dx < 0) {
         dx = -dx;
       }
-      if (
-        state.ball.y + state.ball.dy > 600 - 20 ||
-        state.ball.y + state.ball.dy < 1
-      ) {
+
+      if (y + dy > 400 - 20 || y + dy < 0) {
         dy = -dy;
       }
-      dispatch({
+
+      if (y > 340 && paddleX < x + dx && paddleX + 100 > x + dx) {
+        dy = -dy;
+      }
+
+      return dispatch({
         type: "MOVE_BALL",
         payload: {
           dx,
           dy,
-          x: state.ball.x + dx,
-          y: state.ball.y + dy
+          x: x + dx,
+          y: y + dy
         }
       });
     }, 50);
-    return () => clearTimeout(handleBall);
+    return () => clearTimeout(handle);
   }, [state.ball]);
 
   return (
-    <div className="App">
-      <div className="container">
-        {/* <Container> */}
-        <Paddle paddleY={state.p1Paddle.y} />
-        <Paddle isPlayerTwo paddleY={state.p2Paddle.y} />
-        <Ball pos={state.ball} />
-        {/* </Container> */}
-      </div>
+    <div className="container">
+      <Paddle paddleX={state.paddle.x} />
+      <Ball pos={state.ball} />
     </div>
   );
 }
